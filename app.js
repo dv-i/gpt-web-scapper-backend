e6efd88b58038620da5ddf1090d76b45fa7de1c7
Jaimish Ashar <jaimish97@gmail.com>
2023-07-25 00:39:58 -0400

    Converts port into env variable for Heroku

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import fs from "fs";
 import * as chatgpt from "chatgpt";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
